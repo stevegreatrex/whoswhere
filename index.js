aa8aba335306eb7ca9f74fd60db7513e815914ca
Steve Greatrex <stephen.greatrex@googlemail.com>
2015-01-31 13:04:42 +0000

    Added bower, bootstrap & blank home page

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
+/*global require: false, __dirname: false*/
+
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/server/views'));
+app.use('/content', express.static(__dirname + '/server/content'));
+app.use('/lib', express.static(__dirname + '/bower_components'));
 
 app.get('/', function(req, res) {
   res.render('/server/views/index.html');
 });
 
-var server = app.listen(3000, function() {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Server started on http://%s:%s', host, port);
-});
+var server = app.listen(3000);
+console.log('Server started on http://%s:%s', server.address().address, server.address().port);
