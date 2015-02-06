5fa6ae6f09b6d07c5d1ede7a1f8263e7b7efb362
Steve Greatrex <stephen.greatrex@googlemail.com>
2015-02-06 20:46:41 +0000

    Fixed problem with collapsing menu on smaller screens

--- app/navigation/navigation.js
+++ app/navigation/navigation.js
@@ -1,23 +1,25 @@
 (function(angular) {
 
-    function createLink(name, url, icon) {
-        return {
-            name: name,
-            url: url,
-            icon: icon
-        };
-    }
+	function createLink(name, url, icon) {
+		return {
+			name: name,
+			url: url,
+			icon: icon
+		};
+	}
 
 angular.module('whoswhere.navigation', [])
 
-    .controller('NavCtrl', ['$scope', '$location', function ($scope, $location) {
-        $scope.links = [
-            createLink('Home', '/home', 'home'),
-            createLink('Absences', '/absences', 'calendar')
-        ];
+	.controller('NavCtrl', ['$scope', '$location', function ($scope, $location) {
+		$scope.links = [
+				createLink('Home', '/home', 'home'),
+				createLink('Absences', '/absences', 'calendar')
+		];
 
-        $scope.isActive = function(url) {
-            return $location.path().indexOf(url) === 0;
-        }
-    }]);
+		$scope.collapsed = true;
+
+		$scope.isActive = function(url) {
+				return $location.path().indexOf(url) === 0;
+		}
+	}]);
 }(angular));
